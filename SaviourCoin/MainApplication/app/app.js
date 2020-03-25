403fe4cd35e45732d9f3d0f2f7ecf46bd0ff15c4
Anton Eliasson Gustafsson <antogus@student.chalmers.se>
2020-03-25 16:09:30 +0100

    funktion för att få alla accounts automatiskt

--- SaviourCoin/MainApplication/app/app.js
+++ SaviourCoin/MainApplication/app/app.js
@@ -1,20 +1,33 @@
+
 var Web3 = require('web3');
-var web3 = new Web3('HTTP://127.0.0.1:7545');
+var web3 = new Web3('HTTP://127.0.0.1:8545');
 
 var contractJson = require('../build/contracts/SaviourCoin.json');
 
-var contractAddress = "0x56e1D54506Ba7d0dd36c22059e4ACabcF4f3B291";
+var contractAddress = contractJson.networks[1585140692481].address;
 var contract = new web3.eth.Contract(contractJson.abi, contractAddress);
 
 contract.methods.helloWorld().call(
 	(err, result) => {console.log(result)});
+/*
+var accounts = null;
+async function accs(){
+	var as = await web3.eth.getAccounts()
+	console.log(as[0]);
+	accounts = as;
+}
+*/
+var accounts = null;
+var x = web3.eth.getAccounts(
+	(err, result) => {accounts = result}
+										         );
 
-var acc1 = "0x1D725392C358b5Be4E9944D8C6a8d716a7128434";
-var acc2 = "0xB83E4c4c35698b18CAdeF7Bec22CCDc83CaBEa85";
+console.log(accounts);
+var acc1 = "0x70F750b37cC8df2D3a95e0A3E76A23B45F8099ce";
+var acc2 = "0xbe9F16bE21652E4f1878834cD89488801e2c98FA";
 
 contract.methods.transfer(acc2, 10).send({from: acc1})
 .once('receipt', (receipt) => {console.log("Yes")});
 
 contract.methods.balanceOf(acc1).call(
 	(err, result) => {console.log(result)});
-
