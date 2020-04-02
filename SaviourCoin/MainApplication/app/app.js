6e69dba8781649d95b767cb0ac5279c029ad65a7
Anton Eliasson Gustafsson <antogus@student.chalmers.se>
2020-04-02 02:24:29 +0200

    Async funktioner

--- SaviourCoin/MainApplication/app/app.js
+++ SaviourCoin/MainApplication/app/app.js
@@ -4,30 +4,41 @@ var web3 = new Web3('HTTP://127.0.0.1:8545');
 
 var contractJson = require('../build/contracts/SaviourCoin.json');
 
-var contractAddress = contractJson.networks[1585140692481].address;
+var contractAddress = contractJson.networks[1585781614091].address; // Denna måste hårdkodas in
 var contract = new web3.eth.Contract(contractJson.abi, contractAddress);
 
-contract.methods.helloWorld().call(
-	(err, result) => {console.log(result)});
-/*
-var accounts = null;
-async function accs(){
-	var as = await web3.eth.getAccounts()
-	console.log(as[0]);
-	accounts = as;
+var accounts;
+
+async function setUp(){
+	await getAccs();
+	console.log(accounts[0]);
+	var from = accounts[0];
+	var to = accounts[1];
+	await HelloExjobb();
+	await transfer(from, to);
+	await checkBal(from);
+
 }
-*/
-var accounts = null;
-var x = web3.eth.getAccounts(
-	(err, result) => {accounts = result}
-										         );
 
-console.log(accounts);
-var acc1 = "0x70F750b37cC8df2D3a95e0A3E76A23B45F8099ce";
-var acc2 = "0xbe9F16bE21652E4f1878834cD89488801e2c98FA";
 
-contract.methods.transfer(acc2, 10).send({from: acc1})
-.once('receipt', (receipt) => {console.log("Yes")});
+async function HelloExjobb(){
+	await contract.methods.helloWorld().call(
+		(err, result) => {console.log(result)});
+}
 
-contract.methods.balanceOf(acc1).call(
+async function getAccs(){
+ 		accounts = await web3.eth.getAccounts(
+		(err, result) => {if(err) {console.log("Failed to load accounts.")};});
+}
+
+async function transfer(from, to){
+	await	contract.methods.transfer(to, 10).send({from: from})
+	.once('receipt', (receipt) => {console.log("Yes")});
+}
+
+async function checkBal(acc){
+	await contract.methods.balanceOf(acc).call(
 	(err, result) => {console.log(result)});
+}
+
+setUp();
