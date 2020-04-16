3b0427d7f7288055b732e1fdbfa5b017a45a1eb9
Anton Eliasson Gustafsson <antogus@student.chalmers.se>
2020-04-16 16:41:22 +0200

    La till events
    events för funktioner som skriver på blockkedjan

--- SaviourCoin/MainApplication/app/app.js
+++ SaviourCoin/MainApplication/app/app.js
@@ -15,19 +15,19 @@ var accounts;
 
 async function setUp(){
 	await getAccs();
-	console.log(accounts[0]);
+//	console.log(accounts[0]);
 	var from = accounts[0];
 	var to = accounts[1];
 	var nobody = accounts[2];
 	//await HelloExjobb();
 //	await transfer(from, to);
 //	await checkBal(from);
-//  await addUserToForum(to, "A", 10, from);
-//	await getMembers(to);
-	await createForum("GenesisForum", from);
-	await getForumData(0);
-	await addUserToForum(to, "A", 10, from);
-	await getMembers(nobody);
+
+//	await createForum("GenesisForum", from);
+//  await getMembers(to);
+//	await getForumData(1);
+//	await addUserToForum(to, "First guest", 10, from);
+//  await getMembers(from);
 }
 
 async function getAccs(){
@@ -46,25 +46,32 @@ async function checkBal(acc){
 }
 
 async function addUserToForum(address, username, karma, from){
-	await	forumContract.methods.addUserToForum(address, username, karma).send({from: from})
-	.once('receipt', (receipt) => {console.log('\n' + "Added user: " + receipt.events.AddUserToForum.returnValues._success)});
+	var ev;
+	var res;
+	var msg;
+	await	forumContract.methods.addUserToForum(address, username, karma).send({from: from, gas: 6721975})
+	.once('receipt', (receipt) => {ev = receipt.events.AddUserToForum, res = ev.returnValues.succ, msg = ev.returnValues.info, console.log(res + '\n' + msg)});
+//	.once('receipt', (receipt) => {console.log(JSON.stringify(receipt));});
+//, console.log(res), console.log(msg)
+// res = ev.Result.succ, msg = ev.Result.info,
 }
 
 async function getMembers(address){
-	await forumContract.methods.getUserData(address).call(//{from: accounts[1]},
-	(err, result) => {console.log('\n' + "Memberdata {" + '\n' + "UserAddress: " + result._address + '\n' +
+	await forumContract.methods.getUserData(address).call({gas: 6721975},
+	(err, result) => {console.log('\n' + "Memberdata {" + '\n' + "UserAddress:returnValues " + result._address + '\n' +
 																"UserName: " + result._userinfo + '\n' +
-															  "Karma: " + result._karma + '\n' + "}")});
+															  "Karma: " + result._karma + '\n' +
+																"Result: " + result.succ + '\n' + "}")});
 }
 
 async function createForum(fName, from){
 	await	forumContract.methods.createForum(fName).send({from: from, gas: 6721975 })
-	.once('receipt', (receipt) => {console.log('\n' + "Forum created. ")});
+	.once('receipt', (receipt) => {console.log(receipt.events.CreateForum.returnValues.succ)});
 }
 
 async function getForumData(fid){
 	await forumContract.methods.getForumData(fid).call(
-	(err, result) => {console.log('\n' + result.Owner)});
+	(err, result) => {console.log('\n' + result.succ + '\n' + result.Forum_Name) + '\n'});
 }
 
 setUp();
--- SaviourCoin/MainApplication/contracts/Forum.sol
+++ SaviourCoin/MainApplication/contracts/Forum.sol
@@ -1,11 +1,12 @@
 //Klientsidan
-  //Kunna se alla forum från klientsida.
+  //CHECK! Kunna se alla forum från klientsida.
   //Se till att inte kunna bli tillagd i ett forum hur som helst.
   //Mina uppgifter i ett forum.
-
+ // Skicka säga "ok" att få bli tillagd i ett forum.
 //Forumskaparsida
-  //Skapa ett forum
-  //Lägga till användare i mitt forum.
+  // Kolla om användaren har godkänt att bli tillagd.
+  // Get userCount
+  // GetUser by usercount
 
 //pragma solidity ^0.5.0;
 pragma solidity ^0.5.16;
@@ -40,63 +41,88 @@ contract Forum {
       forumId = 1;
     }
 
-    function createForum(string memory _fname) public returns (uint256 forum_created){
+    function createForum(string memory _fname) public returns (bool success){
+      uint256 a = forums[msg.sender].fid;
+      uint256 b = addressForumId[msg.sender];
+      if((a != 0) && (a == b)){
+        emit CreateForum(false, "You already own a forum! ", msg.sender, _fname, forumId);
+        return(false);
+      }
       forums[msg.sender] = forumData(msg.sender, _fname, forumId, 0);
-      forums[msg.sender].users[1] = userData(msg.sender, _fname, 0);
+      forums[msg.sender].users[0] = userData(msg.sender, _fname, 0);
       forums[msg.sender].userExists[msg.sender] = true;
       forums[msg.sender].userKey[msg.sender] = 0;
       forums[msg.sender].userCount ++;
+      addressForumId[msg.sender] = forumId;
       fidOwner[forumId] = msg.sender;
       forumId = forumId + 1;
+      emit CreateForum(true, "Forum created successfully! ", msg.sender, _fname, forumId);
+      return (true);
+    }
 
+    function getForumCount() public view returns (uint256 id){
       return forumId;
     }
 
-    function getForumId() public view returns (uint256 id){
-      return forumId;
-    }
-
-    function getForumData(uint256 _fid) public view returns (address Owner, string memory Forum_Name, uint256 Forum_ID){
+    function getForumData(uint256 _fid) public view returns (bool succ, address Owner, string memory Forum_Name, uint256 Forum_ID){
       address owner = fidOwner[_fid];
+      if(owner == address(0x0))
+        return (false, owner, "Forum does not exist. ", 0);
       string memory fName = forums[owner].forumName;
       uint256 id = forums[owner].fid;
-      return(owner, fName, id);
+      return(true, owner, fName, id);
+    }
+
+    function getForumData(address _owner) public view returns (bool succ, address Owner, string memory Forum_Name, uint256 Forum_ID){
+      if(addressForumId[msg.sender] == 0){
+        return (false, address(0x0), "Forum does not exists. ", 0);
+      }
+      string memory fName = forums[_owner].forumName;
+      uint256 id = forums[_owner].fid;
+      return(true, _owner, fName, id);
     }
 
     function addUserToForum(address _userAddress, string memory _userName, uint256 _karma) public returns (bool success){
       if(forums[msg.sender].owner != msg.sender){
-        return (msg.sender), "You need to create a forum first", 0);
+        emit AddUserToForum(false, "You need to create a forum first. ", _userAddress, _userName);
+        return (false);
+      }
+      if(forums[msg.sender].userExists[_userAddress] == true){
+        emit AddUserToForum(false, "User already exists. ", _userAddress,  _userName);
+        return (false);
       }
       if(!forums[msg.sender].userExists[_userAddress]){
         forums[msg.sender].userCount++;
         uint256 newUserCount = forums[msg.sender].userCount;
-        forums[msg.sender].users[newUserCount] = userData(_userAddress, _userName, 0);
+        forums[msg.sender].users[newUserCount] = userData(_userAddress, _userName, _karma);
         forums[msg.sender].userExists[_userAddress] = true;
         forums[msg.sender].userKey[_userAddress] = newUserCount;
-        emit AddUserToForum(true, _userAddress, _userName, _karma);
-        return true;
-      }
+        emit AddUserToForum(true, "Member added successfully. ", _userAddress, _userName);
+        return (true);
 
-      emit AddUserToForum(false, _userAddress, _userName, _karma);
-      return false;
+      }
+      emit AddUserToForum(false, "Something went wrong. This error is weird, do you suck? ", _userAddress, _userName);
+      return (false);
     }
 
-    function getUserData(address _userAddress) public view returns (address _address, string memory _userinfo, uint256 _karma) {
-        if(forums[msg.sender].owner = address(0x0)){
-          return (address(0x0), "You need to create a forum first", 0);
+
+    function getUserData(address _userAddress) public view returns (bool succ, address _address, string memory _userinfo, uint256 _karma) {
+        if(forums[msg.sender].owner == address(0x0)){
+          return (false, address(0x0), "You need to create a forum first", 0);
         }
         if(forums[msg.sender].userExists[_userAddress]){
           uint256 uKey = forums[msg.sender].userKey[_userAddress];
           address uaddress = forums[msg.sender].users[uKey].userAddress;
           string memory uname = forums[msg.sender].users[uKey].userName;
           uint256 karm = forums[msg.sender].users[uKey].karma;
-          return (uaddress, uname, karm);
+          return (true, uaddress, uname, karm);
         }
-        return(address(0x0), "This user does not exists in your forum", 0);
-
+        if(!forums[msg.sender].userExists[_userAddress]){
+          return(false, address(0x0), "This user does not exists in your forum", 0);
+        }
+        return(false, address(0x0),"Something went wrong with adding a new member.", 0);
     }
-
-
-    event AddUserToForum(bool _success, address _userAddress, string _userName, uint256 _karma);
-
+    event Succ(bool success);
+    event AddUserToForum(bool succ, string info, address _userAddress, string _userName);
+    event CreateForum(bool succ, string info, address self, string fname, uint256 fid);
 }
