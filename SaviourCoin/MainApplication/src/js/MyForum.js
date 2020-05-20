3505c213e53411204b2977e1e2b02aaf2b4a7834
uhrej <uhrej@student.chalmers.se>
2020-05-20 21:16:10 +0200

    Clean up

--- SaviourCoin/MainApplication/src/js/MyForum.js
+++ SaviourCoin/MainApplication/src/js/MyForum.js
@@ -1,7 +1,7 @@
-import React from 'react'
-import ReactDOM from 'react-dom'
-import 'bootstrap/dist/css/bootstrap.css'
-import equal from 'fast-deep-equal'
+import React from 'react';
+import ReactDOM from 'react-dom';
+import 'bootstrap/dist/css/bootstrap.css';
+import equal from 'fast-deep-equal';
 
 class MyForum extends React.Component {
   constructor(props) {
@@ -12,9 +12,10 @@ class MyForum extends React.Component {
       username: '',
       balance: '',
       ownsForum: false,
-      addUserError: '',
+      addUserMsg: '',
       cashOutPrice: '',
-      setCashOutError: ''
+      setCashOutMsg: '',
+      createForumMsg: ''
     }
 
     this.setup = this.setup.bind(this);
@@ -29,13 +30,11 @@ class MyForum extends React.Component {
  
   setup() {
     this.props.location.data.methods.getForumData(this.props.location.data.user.address).call((error, result) => {
-      console.log(error);
-      console.log(result);
       if(error || !result.forumName) {
         this.setState({ownsForum: false});
       } else {
-        this.setState({ownsForum: true});
         this.setState({
+          ownsForum: true,
           forumName: result.forumName,
           cashOutPrice: result.cop
         });
@@ -43,7 +42,7 @@ class MyForum extends React.Component {
     })
 
     this.props.location.data.methods.balanceOf(this.props.location.data.user.address).call((err, result) => {
-      this.setState({ balance: result })
+      this.setState({ balance: result });
     })
   }
 
@@ -52,50 +51,42 @@ class MyForum extends React.Component {
     if (this.state.price) {
       this.props.location.data.methods.setCashOutPrice(this.state.price)
         .send({from: this.props.location.data.user.address, gas: 6721975}, (error, result) => {
-          console.log(result);
         if (error) {
-          console.log('fail');
-          this.setState({setCashOutError: "To high. Aquire more scones or try a lower COP."});
+          this.setState({setCashOutMsg: "To high. Aquire more scones or try a lower COP."});
         } else {
-          console.log('success');
           this.setState({
             cashOutPrice: this.state.price,
-            setCashOutError: ""
+            setCashOutMsg: ""
           });
         }
       })
     } else {
-      console.log("You must enter a price");
+      this.setState({setCashOutMsg: "You must enter a price"});
     }
   }
 
   addUser(event) {
-    event.preventDefault()
+    event.preventDefault();
     if (this.state.address && this.state.username) {
       try{
         this.props.location.data.methods.addUserToForum(this.state.address, this.state.username, 100)
           .send({from: this.props.location.data.user.address, gas: 6721975}, (error, result) => {
-            console.log(error);
-            console.log(result);
-
-          if(result) {
+          if(error) {
+            this.setState({ addUserMsg: "User could not be added" });
+          } else {
             this.setState({ 
-              addUserError: "User was added",
+              addUserMsg: "User was added",
               address: '',
               username: ''
             });
-            console.log("User was added");
-          } else {
-            this.setState({ addUserError: "User could not be added (Else)" });
-            console.log("Error: User could not be added (Else)")
           }
         })
       } catch (e) {
         console.log(e);
-        this.setState({ addUserError: "The user does not exist" });
+        this.setState({ addUserMsg: "The user does not exist" });
       }
     } else {
-      this.setState({ addUserError: "You must enter an address and a username" });
+      this.setState({ addUserMsg: "You must enter an address and a username" });
     }
   }
 
@@ -104,16 +95,15 @@ class MyForum extends React.Component {
     if (this.state.forumName) {
       this.props.location.data.methods.createForum(this.state.forumName)
         .send({from: this.props.location.data.user.address, gas: 6721975}, (error, result) => {
-          console.log(error);
-        if (result) {
+        if (error) {
+          console.log('fail');
+        } else {
           console.log('success');
           this.setState({ownsForum: true});
-        } else {
-          console.log('fail');
         }
       })
     } else {
-      console.log("You must enter a Forum name");
+      this.setState({createForumMsg: "You must enter a Forum name"});
     }
   }
 
@@ -135,7 +125,7 @@ class MyForum extends React.Component {
             <form onSubmit={this.setCashOutPrice}>
                 <label>Price in scones/karma</label>
                 <input type="text" className="form-control" value={this.state.price} onChange={e => this.logChange(e, "price")}/>
-                <p>{this.state.setCashOutError}</p>
+                <p>{this.state.setCashOutMsg}</p>
                 <div className="submit-section">
                     <button className="btn btn-uth-submit">Set</button>
                 </div>
@@ -149,7 +139,7 @@ class MyForum extends React.Component {
                 <input type="text" className="form-control" value={this.state.username} onChange={e => this.logChange(e, "username")}/>
                 <label>User Address</label>
                 <input type="text" className="form-control" value={this.state.address} onChange={e => this.logChange(e, "address")}/>
-                <p>{this.state.addUserError}</p>
+                <p>{this.state.addUserMsg}</p>
                 <div className="submit-section">
                     <button className="btn btn-uth-submit">Add user</button>
                 </div>
@@ -165,6 +155,7 @@ class MyForum extends React.Component {
             <form onSubmit={this.createForum}>
                 <label>Forum name:</label>
                 <input type="text" className="form-control" onChange={e => this.logChange(e, "forumName")}/>
+                <p>{this.state.createForumMsg}</p>
                 <div className="submit-section">
                     <button className="btn btn-uth-submit">Create</button>
                 </div>
